a4b25bd771519fee01bc34c94b1c8263785c106d
VolodymyrPlastun <vplastun8@gmail.com>
2022-02-20 08:56:09 +0200

    Внес правки от ментора

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -9,7 +9,7 @@ galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
 function createGalleryMarkup(galleryItems) {
     return galleryItems.map(({preview, original, description}) => {
      return `<div class="gallery__item">
-  <a class="gallery__link" href="${original}" onclick="event.preventDefault()">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
@@ -23,34 +23,40 @@ function createGalleryMarkup(galleryItems) {
 
 galleryContainer.addEventListener('click', onImgClick);
 
+const instance = basicLightbox.create(`
+ <img class="modal-img" src="">`,
+ {
+  onShow: (instance) => {
+   window.addEventListener('keydown', onEscBtnClose);
+},
+},
+    {
+  onClose: (instance) => {
+    window.removeEventListener('keydown', onEscBtnClose);
+  },
+},
+);
+
 function onImgClick(evt) {
-  const imgUrl = evt.target.dataset.source;
-  const instance = basicLightbox.create(`
- <img src="${imgUrl}" width="800" height="600">
-    
-`)
-  instance.show()
-  // {
-  //   onShow: (instance) => {
-      window.addEventListener('keydown', onModalClose, { once: true });
-      function onModalClose(event) {
-        console.log(event.code);
-        if (event.code === 'Escape') {
-      
-          instance.close()
+  evt.preventDefault();
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+  instance.element().querySelector('.modal-img').src = evt.target.dataset.source;
+
+  instance.show();
+
+    }
+
+      function onEscBtnClose(evt) {
+        
+        if (evt.code === 'Escape') {
+          instance.close();
+          return;
         }
       }
-    }
-// }
-// }
-
 
   
-// {
-//   onClose: (instance) => {
-//     window.removeEventListener('keydown', onModalClose);
-//   }
-// }
 
 
 
